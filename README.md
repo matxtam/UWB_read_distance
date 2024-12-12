f3c8aa8f0e785baac37de50e2f755baec49fb57a
matxtam <hd.reservoir@gmail.com>
2024-12-12 18:20:34 +0800

    12/12

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 python3 -m venv venv
 source venv/vin/activate
 
+// for _mqtt.py
 pip install python-dotenv awsiotsdk
+
+// for read_GIPS_uwb.py
+pip install pyserial pandas numpy
 ```
 
 ## Run code (_mqtt.py)
--- read_GIPS_distance.py
+++ read_GIPS_distance.py
@@ -5,10 +5,25 @@ import pandas as pd
 import collections
 import time
 import socket
+# COM_PORT = 'dev/ttyUSB0'  # for rpi
+COM_PORT = '/dev/ttyS3'  # for rpi
+# COM_PORT = 'COM4'   # for computer
+anchor_IDs = ['0241000000000000','0341000000000000','0441000000000000','0541000000000000']
+BAUD_RATES = 57600    
+ser_UWB = serial.Serial(COM_PORT, BAUD_RATES) 
+diss = np.zeros(4)
 
-const COM_PORT = 'dev/ttyUSB0'  # for rpi
-# const COM_PORT = 'COM0'   # for computer
-const anchor_IDs = ['0241000000000000','0341000000000000','0441000000000000','0541000000000000']
+x1 = 1
+y1 = 2
+x2 = 3
+y2 = 4
+x3 = 5
+y3 = 6
+
+X = np.array([x1, x2, x3])
+Y = np.array([y1, y2, y3])
+XY = X.cross(Y).dot(np.array([1, 1, 1]))
+C0 = np.array([(x1*x1 + y1*y1),(x2*x2 + y2*y2), (x3*x3 + y3*y3) ])
 
 def swapEndianness(hexstring):
 	ba = bytearray.fromhex(hexstring)
@@ -17,13 +32,11 @@ def swapEndianness(hexstring):
 
 def UWB_dis():
 
-    BAUD_RATES = 57600    
-    ser_UWB = serial.Serial(COM_PORT, BAUD_RATES) 
-    dis_queue = collections.deque(maxlen = 1)
-    t = time.time()
+    # dis_queue = collections.deque(maxlen = 1)
+    # t = time.time()
     rx = ser_UWB.read(66) # original 264 = 66*4
     rx = binascii.hexlify(rx).decode('utf-8')
-    global diss = np.zeros(4)
+    global diss
     
     for index, anchor_ID in anchor_IDs:
         if( rx != ' ' and rx.find(anchor_ID) >= 0 and rx.find(anchor_ID) <= (len(rx)-24)):
@@ -41,28 +54,31 @@ def UWB_dis():
         else:
             dis = 0
         diss[index] = dis
+        
     return diss
 
-def test():
-
 def _main():
+    data_filename = 'UWB_distance.csv'    
     dis_to_tag_ls = []
     count = 0
     try:
         while(count < 100):
-
             dis_to_tag = UWB_dis()
+            print("anchor ID 0? " + str(dis_to_tag[0]), end="\t")
+            print("anchor ID 7: " + str(dis_to_tag[1]), end="\t")
+            print("anchor ID 2? " + str(dis_to_tag[2]), end="\t")
+            print("anchor ID 3? " + str(dis_to_tag[3]))
+            r1 = dis_to_tag[1]
+            r2 = dis_to_tag[2]
+            r3 = dis_to_tag[3]
+            C = C0 - np.array([r1*r1, r2*r2, r3*r3])
+            CY = C.cross(Y).dot(np.array([1, 1, 1]))
+            XC = X.cross(C).dot(np.array([1, 1, 1]))
+            x = CY / XY / 2
+            y = XC / XY / 2
+            print("(x, y) = ({}, {})".format(x, y))
+            
 
-            if(0 not in dis_to_tag):
-                count = count + 1
-
-                a2_value = dis_to_tag[0] 
-                a3_value = dis_to_tag[1] 
-                a4_value = dis_to_tag[2] 
-                a5_value = dis_to_tag[3]
-                print('anchor ID 7:' + a3_value) 
-                dis_to_tag = [a2_value, a3_value, a4_value, a5_value]
-                dis_to_tag_ls.append(dis_to_tag)
            
     except KeyboardInterrupt:
 
